a0c28b899aa3525f3101e0263a38d8de6c59139b
Vladimir Kalinkin <vova.kalinkin@gmail.com>
2021-11-11 02:32:32 +0300

    Initial version.

---
+++ lib/dandy/kafka.rb
@@ -0,0 +1,8 @@
+require "dandy/kafka/version"
+
+module Dandy
+  module Kafka
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/dandy/kafka/version.rb
@@ -0,0 +1,5 @@
+module Dandy
+  module Kafka
+    VERSION = "0.1.0"
+  end
+end
